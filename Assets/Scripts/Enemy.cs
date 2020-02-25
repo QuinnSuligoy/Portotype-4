1de91337e67d5a4c8bdb40d566f55d175f99e48e
QuinnSuligoy <56174399+QuinnSuligoy@users.noreply.github.com>
2020-02-25 11:34:35 -0800

    Fisnished

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 3;
+    private Rigidbody enemyRB;
+    private GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRB = GetComponent<Rigidbody>();
+        Player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookdirection = ((Player.transform.position - transform.position).normalized);
+        enemyRB.AddForce(lookdirection * speed);
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private float spawnrange = 9.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(enemyPrefab, GenerateSpawnPos(), enemyPrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Vector3 GenerateSpawnPos()
+    {
+        float spawnposX = Random.Range(spawnrange, -spawnrange);
+        float spawnposZ = Random.Range(spawnrange, -spawnrange);
+        Vector3 randompos = new Vector3(spawnposX, 0, spawnposZ);
+        
+        return randompos;
+    }
+}
